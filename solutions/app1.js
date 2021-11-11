fd125f036eb86f2656adf0496a06eb3895928ad3
Gabor Szasz <skywallalp@gmail.com>
2021-11-11 01:36:56 +0100

    export, import for test

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,7 @@
+const setCookie = (cookieValue) => {
+  const expires = new Date(Date.now() + 15 * 60 * 1000);
+  document.cookie = `token= ${cookieValue}; expires= ${expires.toUTCString()}; path=/`;
+};
 
+// export default setCookie;
+export default setCookie;
